777972c67322d037b06b3af195233ca81a299116
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2026-09-08 12:23:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestConsecutive(int[] arr) {
+        return longest(arr);
+        
+    }
+    public int longest(int[] nums) {
+            if(nums.length == 0){
+                return 0;
+            }
+            HashSet<Integer> set = new HashSet<>();
+            for(int item : nums){
+                set.add(item);
+            }
+
+            int longest = 1;
+
+            for(int item : set){
+                if(set.contains(item-1)){
+                    continue;
+                }
+                int cnt = 1;
+                int curr = item;
+                while(set.contains(curr+1)){
+                    cnt++;
+                    curr++;
+                }
+                longest = Math.max(cnt, longest);
+            }
+            return longest;
+        }
+}
